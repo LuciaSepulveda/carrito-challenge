5d8d6b47e78e807c6e1d81ed721b1ad7e454d967
LuciaSepulveda <luciabsep18@gmail.com>
2023-07-11 19:10:45 -0300

    fetch the products and save them in a state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
-import { CarritoComponent } from "./components/CarritoComponent";
-import { HeaderComponent } from "./components/HeaderComponent";
-import { ListadoProductosComponent } from "./components/ListadoProductosComponent";
+import { useEffect, useState } from "react"
+import { CarritoComponent } from "./components/CarritoComponent"
+import { HeaderComponent } from "./components/HeaderComponent"
+import { ListadoProductosComponent } from "./components/ListadoProductosComponent"
+import { Product } from "./types"
 
 function App() {
-  const [showCarrito, setShowCarrito] = useState(false);
+  const [showCarrito, setShowCarrito] = useState(false)
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/productos")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data)
+      })
+      .catch((error) => console.error(error))
+  }, [])
+
   return (
     <div
       className="min-h-full bg-fixed"
@@ -13,11 +25,11 @@ function App() {
       <HeaderComponent />
       <div className="flex justify-center min-h-full">
         <div className="max-w-lg w-full py-16">
-          {showCarrito ? <CarritoComponent /> : <ListadoProductosComponent />}
+          {showCarrito ? <CarritoComponent /> : <ListadoProductosComponent products={products} />}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/ListadoProductosComponent.tsx
+++ src/components/ListadoProductosComponent.tsx
@@ -1,7 +1,19 @@
-export const ListadoProductosComponent = () => {
+import { Product } from "../types"
+
+interface Props {
+  products: Product[]
+}
+
+export const ListadoProductosComponent = ({ products }: Props) => {
   return (
     <div>
       <span>ListadoProductosComponent</span>
+      {products.map((product) => (
+        <div>
+          {product.id}
+          {product.nombre}
+        </div>
+      ))}
     </div>
-  );
-};
+  )
+}
