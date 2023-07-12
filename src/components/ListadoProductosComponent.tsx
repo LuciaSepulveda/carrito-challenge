56cd24e8375590e148b5a0eb496f6fbc46caa623
LuciaSepulveda <luciabsep18@gmail.com>
2023-07-12 14:58:28 -0300

    add styles to product list and use hooks for cart and gemas

--- src/components/ListadoProductosComponent.tsx
+++ src/components/ListadoProductosComponent.tsx
@@ -1,3 +1,4 @@
+import { useAddToCart, useCart, useGemas } from "../context/hooks"
 import { Product } from "../types"
 
 interface Props {
@@ -5,13 +6,43 @@ interface Props {
 }
 
 export const ListadoProductosComponent = ({ products }: Props) => {
+  const cart = useCart()
+  const addToCart = useAddToCart()
+  const gemas = useGemas()
+
   return (
-    <div>
-      <span>ListadoProductosComponent</span>
+    <div className="w-full flex flex-row flex-wrap gap-8">
       {products.map((product) => (
-        <div>
-          {product.id}
-          {product.nombre}
+        <div
+          key={product.id}
+          className="relative bg-stone-700 h-72 w-60 flex flex-col justify-between p-8 rounded-xl border border-stone-700 hover:border-purple-700"
+        >
+          <p className="top-4 right-4 absolute bg-green-600 w-fit px-3 py-0.5 rounded-2xl text-xs self-end">
+            {product.precio} Gema
+          </p>
+          <img
+            src={product.imagen}
+            alt={product.nombre}
+            className="w-20 self-center -mt-1"
+          />
+          <div>
+            <p className="font-semibold mt-2">{product.nombre}</p>
+            <p className="text-slate-500 text-sm">{product.descripcion}</p>
+          </div>
+          <button
+            className="w-full bg-purple-700 text-white py-1 rounded-xl disabled:bg-stone-500"
+            disabled={
+              cart?.items !== undefined &&
+              (cart?.items.find(
+                (elem: Product) =>
+                  elem.id === product.id || elem.categoria === product.categoria
+              ) !== undefined ||
+                product.precio > gemas)
+            }
+            onClick={() => addToCart(product)}
+          >
+            Agregar
+          </button>
         </div>
       ))}
     </div>
